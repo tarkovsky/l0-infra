af2ed3045b788f42d187a27dc6f48d0b691b00f9
horance <horance@outlook.com>
2015-04-05 21:31:25 +0800

    support GCC4.6

--- include/l0-infra/base/Keywords.h
+++ include/l0-infra/base/Keywords.h
@@ -2,9 +2,16 @@
 #define H37F8D484_D7F2_4FB6_A88F_0ED12D5C0CB2
 
 #include <l0-infra/base/Default.h>
+#include <l0-infra/base/Config.h>
 
 #define ABSTRACT(...) virtual __VA_ARGS__ = 0
-#define OVERRIDE(...) virtual __VA_ARGS__ override
+
+#if __SUPPORT_VIRTUAL_OVERRIDE
+# define OVERRIDE(...) virtual __VA_ARGS__ override
+#else
+# define OVERRIDE(...) virtual __VA_ARGS__
+#endif
+
 #define EXTENDS(...) , ##__VA_ARGS__
 #define IMPLEMENTS(...) EXTENDS(__VA_ARGS__)
 
